f67991f4433d2c73c1ba229fc9d33559c2eb9d89
wajahat <wajahat.ahmed245@gmail.com>
2019-04-07 23:45:39 +0500

    Initial commit

---
+++ WES/Audience/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, "audience/index.html")
